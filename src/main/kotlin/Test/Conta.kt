f516a55c67c0ce75fbc1285f4591fec9171ddc4f
Neoneo28 <fakihallouche@gmail.com>
2021-07-31 10:49:30 -0300

    Teste

---
+++ src/main/kotlin/Test/Conta.kt
@@ -0,0 +1,17 @@
+package Test
+
+import java.math.BigDecimal
+
+class Conta(
+    val Agencia: String,
+    val Numéro: String,
+    val Saldo: BigDecimal
+) {
+    fun Deposito(valor: BigDecimal) {
+
+    }
+
+    fun Saque(valor: BigDecimal) {
+
+    }
+}
