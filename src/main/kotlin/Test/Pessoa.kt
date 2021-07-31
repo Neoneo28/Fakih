fb6fdbeb2773abd61bd289ed109e9230afc17835
Neoneo28 <fakihallouche@gmail.com>
2021-07-31 01:17:16 -0300

    git add "Testes de como não alterar o CPF"

--- src/main/kotlin/Test/Pessoa.kt
+++ src/main/kotlin/Test/Pessoa.kt
@@ -2,9 +2,11 @@ package Test
 
 class Pessoa {
     var Nome: String = " Ali Fakih Junior"
-    var CPF: String = " CPF: 345.765.567-67"
 
-    inner class Endereco {
+    var CPF: String = " CPF: 345.765.567-67"
+    private set
+
+    inner class Endereço {
         var Rua: String = "Rua teste"
     }
 }
@@ -12,8 +14,9 @@ class Pessoa {
 fun main() {
     val Ali = Pessoa()
 
+    println(Ali)
     println(Ali.Nome)
     println(Ali.CPF)
 
-    println(Ali.Endereco().Rua)
+    println(Ali.Endereço().Rua)
 }
