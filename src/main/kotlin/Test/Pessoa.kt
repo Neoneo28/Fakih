ec8ed82ae6b6954d830f3eec79d0dc781c8fd976
Neoneo28 <fakihallouche@gmail.com>
2021-07-31 00:43:51 -0300

    git add "adicinar Fakih"

---
+++ src/main/kotlin/Test/Pessoa.kt
@@ -0,0 +1,12 @@
+package Test
+
+class Pessoa {
+    var Nome: String = " Ali Fakih Junior"
+    var CPF: String = " CPF: 545.930.852-68"
+}
+fun main() {
+    val Ali = Pessoa()
+
+    println(Ali.Nome)
+    println(Ali.CPF)
+}
